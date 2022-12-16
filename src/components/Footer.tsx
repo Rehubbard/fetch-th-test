598d3cfe7da284157fe4c0e551ea70453d3f7079
Eric Hubbard <erichubbard@erics-mbp.lan>
2022-12-16 10:42:39 -0600

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => (
+  <div className="bg-neutral-800 flex items-center justify-center pt-8 pb-4">
+    <p className="text-white">
+      Created by{" "}
+      <a className="underline" target="_blank" href="https://ehubbard.dev">
+        Eric Hubbard
+      </a>
+      , Lead Egg Nog Enthusiast. © 2022
+    </p>
+  </div>
+);
+
+export default Footer;
